a5a64f0fc927e56b2fc4f13dc5c72a86f758d0b2
aqilrahmat3 <144086802+aqilrahmat3@users.noreply.github.com>
2024-05-20 20:06:59 +0700

    Add files via upload

---
+++ Jobsheet 10/Pasien05.java
@@ -0,0 +1,13 @@
+public class Pasien05 {
+    String nama;
+    int noID, umur;
+    char jenisKelamin;
+    
+
+    public Pasien05(String nama, int noID, char jenisKelamin, int umur) {
+        this.nama = nama;
+        this.noID = noID;
+        this.jenisKelamin = jenisKelamin;
+        this.umur = umur;
+    }
+}
